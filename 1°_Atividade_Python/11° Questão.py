4a03eb2a4a834b8755c8c9c9a2501d524d764573
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 16:04:46 -0300

    11° Questão.py

--- 1°_Atividade_Python/11° Questão.py
+++ 1°_Atividade_Python/11° Questão.py
@@ -1,1 +1,16 @@
+#11 - Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+#* o produto do dobro do primeiro com metade do segundo .
+#* a soma do triplo do primeiro com o terceiro.
+#* o terceiro elevado ao cubo.
 
+num_int1 = int(input("Digite o primeiro número inteiro: "))
+num_int2 = int(input("Digite o segundo número inteiro: "))
+num_real = float(input("Digite um número real: "))
+
+produto = (2 * num_int1) * (num_int2 / 2)
+soma = (3 * num_int1) + num_real
+cubo = num_real ** 3
+
+print(f"O produto do dobro do primeiro com metade do segundo é: {produto}")
+print(f"A soma do triplo do primeiro com o terceiro é: {soma}")
+print(f"O terceiro elevado ao cubo é: {cubo}")
