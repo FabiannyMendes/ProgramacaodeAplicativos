1d5eb8daf2b46099050305f6a4258da34e984688
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 15:28:04 -0300

    10° Questão.py

--- 1°_Atividade_Python/10° Questão.py
+++ 1°_Atividade_Python/10° Questão.py
@@ -1,1 +1,5 @@
+#10 - Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
 
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"A temperatura em Fahrenheit é: {fahrenheit:.2f}°F")
