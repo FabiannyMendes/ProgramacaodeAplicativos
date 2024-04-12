c85269c514d507e2d0aa6a16217094a0e6aea634
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:47:37 -0300

    04° Questão.py

--- 5°_Atividade_de_Python/04° Questão.py
+++ 5°_Atividade_de_Python/04° Questão.py
@@ -1,1 +1,19 @@
+#4 - Escreva uma função chamada inverte_string que aceita uma string como parâmetro e retorna a string invertida.
 
+def inverte_string(string):
+    """
+    Inverte uma string.
+
+    Argumentos:
+    string: Uma string.
+
+    Retorna:
+    A string invertida.
+    """
+    return string[::-1]
+
+
+texto = "Olá, mundo!"
+texto_invertido = inverte_string(texto)
+print("String original:", texto)
+print("String invertida:", texto_invertido)
