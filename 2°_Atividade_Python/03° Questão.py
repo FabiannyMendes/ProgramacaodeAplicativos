ed85391b016c7254ef7e03d2f80ae2f0fbd994cb
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:04:21 -0300

    03° Questão.py

--- 2°_Atividade_Python/03° Questão.py
+++ 2°_Atividade_Python/03° Questão.py
@@ -1,1 +1,14 @@
+#3 - Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 
+
+letra = input("Digite uma letra: ")
+
+letra = letra.lower()
+
+if letra.isalpha():
+    if letra in 'aeiou':
+        print(f'A letra {letra} é uma vogal.')
+    else:
+        print(f'A letra {letra} é uma consoante.')
+else:
+    print('Por favor, digite apenas uma letra do alfabeto.')
