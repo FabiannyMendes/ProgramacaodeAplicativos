b8ba775a3b3e0d3495a948e8916fec5faa047893
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:48:16 -0300

    05° Questão.py

--- 5°_Atividade_de_Python/05° Questão.py
+++ 5°_Atividade_de_Python/05° Questão.py
@@ -1,1 +1,24 @@
+#5 - Escreva uma função chamada maior_valor que aceita uma lista de números como parâmetro e retorna o maior valor na lista.
 
+def maior_valor(lista):
+    """
+    Retorna o maior valor em uma lista de números.
+
+    Argumentos:
+    lista: Uma lista de números.
+
+    Retorna:
+    O maior valor na lista.
+    """
+    if not lista:
+        return None
+
+    max_valor = lista[0]
+    for num in lista:
+        if num > max_valor:
+            max_valor = num
+    return max_valor
+
+
+numeros = [10, 5, 20, 15, 30]
+print("O maior valor na lista é:", maior_valor(numeros))
