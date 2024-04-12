1245bbfa8b3c17ec0831d85b87d478fdd4175541
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:14:08 -0300

    03° Questão.py

--- 3°_Atividade_Python/03° Questão.py
+++ 3°_Atividade_Python/03° Questão.py
@@ -1,1 +1,9 @@
+#3 - Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
 
+print("Imprimir abaixo:")
+for i in range(1, 21):
+    print(i)
+
+print("\nImprimir ao lado:")
+for i in range(1, 21):
+    print(i, end=" ")
