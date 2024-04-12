0b4b250f3c9f1f5a1649b8bede337057f39cd8d4
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:39:39 -0300

    02° Questão.py

--- 4°_Atividade_Python/02° Questão.py
+++ 4°_Atividade_Python/02° Questão.py
@@ -1,1 +1,6 @@
+#2 - Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos quadrados dos elementos do vetor.
 
+vetor_a = [int(input(f"Digite o número {i + 1}: ")) for i in range(10)]
+
+soma_quadrados = sum(x**2 for x in vetor_a)
+print(f"\nA soma dos quadrados dos elementos do vetor é: {soma_quadrados}")
