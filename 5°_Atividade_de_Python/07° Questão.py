cd3f6277bdc5576083a17578f385687274b8589d
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:49:45 -0300

    07° Questão.py

--- 5°_Atividade_de_Python/07° Questão.py
+++ 5°_Atividade_de_Python/07° Questão.py
@@ -1,1 +1,19 @@
+#7 - Escreva uma função chamada soma_quadrados que aceita uma lista de números como parâmetro e retorna a soma dos quadrados desses números.
 
+def soma_quadrados(lista):
+    """
+    Calcula a soma dos quadrados dos números em uma lista.
+
+    Argumentos:
+    lista: Uma lista de números.
+
+    Retorna:
+    A soma dos quadrados dos números na lista.
+    """
+    soma = 0
+    for num in lista:
+        soma += num ** 2
+    return soma
+
+numeros = [1, 2, 3, 4, 5]
+print("A soma dos quadrados dos números na lista é:", soma_quadrados(numeros))
