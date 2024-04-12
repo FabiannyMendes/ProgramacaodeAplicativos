c41b1b7e5d8d0a4163a676aa7a134d79d0d14e27
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:41:36 -0300

    04° Questão.py

--- 4°_Atividade_Python/04° Questão.py
+++ 4°_Atividade_Python/04° Questão.py
@@ -1,1 +1,9 @@
+#4 - Altere o programa anterior, intercalando 3 vetores de 10 elementos cada.
 
+vetor1 = [int(input(f"Digite o elemento {i + 1} do primeiro vetor: ")) for i in range(10)]
+vetor2 = [int(input(f"Digite o elemento {i + 1} do segundo vetor: ")) for i in range(10)]
+vetor3 = [int(input(f"Digite o elemento {i + 1} do terceiro vetor: ")) for i in range(10)]
+
+vetor_intercalado = [val for triple in zip(vetor1, vetor2, vetor3) for val in triple]
+
+print("\nVetor intercalado:", vetor_intercalado)
