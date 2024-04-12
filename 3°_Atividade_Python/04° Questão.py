7dc4000f6ed0176d4a6ddcf04b3e75de094275e5
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:14:55 -0300

    04° Questão.py

--- 3°_Atividade_Python/04° Questão.py
+++ 3°_Atividade_Python/04° Questão.py
@@ -1,1 +1,11 @@
+#4 - Faça um programa que leia 5 números e informe o maior número.
 
+maior_numero = None
+
+for i in range(5):
+    numero = float(input(f'Digite o {i+1}º número: '))
+
+    if maior_numero is None or numero > maior_numero:
+        maior_numero = numero
+
+print(f'O maior número é: {maior_numero}')
