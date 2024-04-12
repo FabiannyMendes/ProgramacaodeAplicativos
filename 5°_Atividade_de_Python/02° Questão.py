9b406e95b420d081a19f4dc998eed2f64651ecf1
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:46:03 -0300

    02° Questão.py

--- 5°_Atividade_de_Python/02° Questão.py
+++ 5°_Atividade_de_Python/02° Questão.py
@@ -1,1 +1,24 @@
+#2 - Escreva uma função chamada fatorial que calcula o fatorial de um número inteiro fornecido como argumento
+
+def fatorial(n):
+    """
+    Calcula o fatorial de um número inteiro n.
+
+    Argumentos:
+    n: Um número inteiro.
+
+    Retorna:
+    O fatorial de n.
+    """
+    if n == 0:
+        return 1
+    else:
+        resultado = 1
+        for i in range(1, n + 1):
+            resultado *= i
+        return resultado
+
+
+numero = 5
+print("O fatorial de", numero, "é:", fatorial(numero))
 
