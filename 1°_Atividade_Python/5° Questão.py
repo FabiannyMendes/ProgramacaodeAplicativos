2d65526b7a8864f6fac565a4d508972633185e1a
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 15:18:42 -0300

    5° Questão.py

---
+++ 1°_Atividade_Python/5° Questão.py
@@ -0,0 +1,5 @@
+#5 - Faça um Programa que converta metros para centímetros.
+
+metros = float(input("Digite a medida em metros: "))
+centimetros = metros * 100
+print(f"{metros} metros é igual a {centimetros} centímetros")
