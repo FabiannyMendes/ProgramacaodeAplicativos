a43971c281317d413f8820cd14e5e4a06114091a
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:35:00 -0300

    10° Questão.py

--- 3°_Atividade_Python/10° Questão.py
+++ 3°_Atividade_Python/10° Questão.py
@@ -1,1 +1,15 @@
+#10 - Faça um programa que peça 10 números inteiros, calcule e mostre a quantidade de números pares e a quantidade de números impares.
 
+numeros_pares = 0
+numeros_impares = 0
+
+for i in range(10):
+    numero = int(input(f"Digite o {i + 1}º número inteiro: "))
+
+    if numero % 2 == 0:
+        numeros_pares += 1
+    else:
+        numeros_impares += 1
+
+print(f"\nQuantidade de números pares: {numeros_pares}")
+print(f"Quantidade de números ímpares: {numeros_impares}")
