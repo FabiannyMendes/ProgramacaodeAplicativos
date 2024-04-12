75f2ea67bcd454efce0af12763bf51882302de82
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 16:06:02 -0300

    12° Questão.py

--- 1°_Atividade_Python/12° Questão.py
+++ 1°_Atividade_Python/12° Questão.py
@@ -1,1 +1,13 @@
+#12 - João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. 
+#Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve 
+#pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável  (peso de peixes) e calcule 
+#o excesso. Gravar na variável  a quantidade de quilos além do limite e na variável  o valor da multa que João deverá pagar. Imprima os
+#dados do programa com as mensagens adequadas.
 
+peso_peixes = float(input("Digite o peso dos peixes em quilos: "))
+limite_peso = 50
+excesso = max(0, peso_peixes - limite_peso)
+multa = excesso * 4
+
+print(f"Quantidade de quilos além do limite: {excesso:.2f} kg")
+print(f"Valor da multa a ser pago: R${multa:.2f}")
