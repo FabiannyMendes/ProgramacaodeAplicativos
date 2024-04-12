85d95dfdc93e0bedc4ec0826edb3cf6b24daac1d
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:18:41 -0300

    05° Questão.py

--- 3°_Atividade_Python/05° Questão.py
+++ 3°_Atividade_Python/05° Questão.py
@@ -1,1 +1,13 @@
+#5 - Faça um programa que leia 5 números e informe a soma e a média dos números.
 
+soma = 0
+media = 0
+
+for i in range(5):
+    numero = float(input(f"Digite o {i + 1}º número: "))
+    soma += numero
+
+media = soma / 5
+
+print(f"Soma dos números: {soma}")
+print(f"Média dos números: {media}")
