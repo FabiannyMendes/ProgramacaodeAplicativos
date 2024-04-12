17f86bf99e4f9a0031544a953c58cb9b1310df74
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:35:51 -0300

    11° Questão.py

--- 3°_Atividade_Python/11° Questão.py
+++ 3°_Atividade_Python/11° Questão.py
@@ -1,1 +1,13 @@
+#11 - A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série até o n−ésimo termo.
 
+def fibonacci(n):
+    fib_series = [1, 1]
+
+    while len(fib_series) < n:
+        fib_series.append(fib_series[-1] + fib_series[-2])
+
+    return fib_series[:n]
+
+n = 10
+resultado = fibonacci(n)
+print(f"A série de Fibonacci até o {n}-ésimo termo é: {resultado}")
