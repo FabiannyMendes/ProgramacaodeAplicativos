f2582d9ab7fd9633895c7c1c1f30724dcf25d089
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:44:42 -0300

    01° Questão.py

--- 5°_Atividade_de_Python/01° Questão.py
+++ 5°_Atividade_de_Python/01° Questão.py
@@ -1,1 +1,18 @@
+#1 - Faça um programa para imprimir:
+#1
+# 2 2
+# 3 3 3
+#.....
+ #n n n n n n ... n
+#para um  informado pelo usuário. Use uma função que receba um valor  inteiro e imprima até a n-ésima linha.
 
+
+def imprimir_padrao(n):
+    for i in range(1, n + 1):
+        linha = ' '.join(str(i) * i)
+        print(linha)
+
+
+n = int(input("Digite o valor de n: "))
+
+imprimir_padrao(n)
