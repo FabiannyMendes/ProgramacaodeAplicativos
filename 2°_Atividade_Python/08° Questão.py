00ee383305a7dcd92be89a8b80c5ae59f1a470af
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 17:09:24 -0300

    08° Questão.py

--- 2°_Atividade_Python/08° Questão.py
+++ 2°_Atividade_Python/08° Questão.py
@@ -1,1 +1,19 @@
+#8 - Faça um Programa que leia três números e mostre-os em ordem decrescente.
 
+
+def ordenar_decrescente(num1, num2, num3):
+    numeros = [num1, num2, num3]
+    numeros.sort(reverse=True)
+    return numeros
+
+def main():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    num3 = float(input("Digite o terceiro número: "))
+
+    numeros_ordenados = ordenar_decrescente(num1, num2, num3)
+
+    print("Números em ordem decrescente:", numeros_ordenados)
+
+if __name__ == "__main__":
+    main()
