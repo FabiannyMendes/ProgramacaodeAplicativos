76264e78877d97bd33c68e13610dc3f3b911ec17
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-12 16:02:23 -0300

    08° Questão.py

--- 1°_Atividade_Python/08° Questão.py
+++ 1°_Atividade_Python/08° Questão.py
@@ -1,4 +1,5 @@
-#8 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+#8 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+#Calcule e mostre o total do seu salário no referido mês.
 
 valor_por_hora = float(input("Digite o valor que você ganha por hora: "))
 horas_trabalhadas = float(input("Digite o número de horas trabalhadas no mês: "))
