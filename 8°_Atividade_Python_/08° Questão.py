7e21fe8a8fe2948b659a546f02f2fd7f29859ab7
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-13 15:41:20 -0300

    08° Questão.py

--- 8°_Atividade_Python_/08° Questão.py
+++ 8°_Atividade_Python_/08° Questão.py
@@ -1,1 +1,22 @@
+#8. Crie uma classe que modele uma conta corrente (a) Atributos: numero da conta, nome do correntista e saldo (b) Metodos: alterar nome, deposito e saque; No construtor, saldo e opcional, com valor default zero e os demais atributos sao obrigatorios.
 
+class ContaCorrente:
+  def _init_(self, numero_conta, nome_correntista, saldo=0):
+    self.numero_conta = numero_conta
+    self.nome_correntista = nome_correntista
+    self.saldo = saldo
+
+  def alterar_nome(self, novo_nome):
+    self.nome_correntista = novo_nome
+
+  def deposito(self, valor):
+    self.saldo += valor
+
+  def saque(self, valor):
+    if valor <= self.saldo:
+      self.saldo -= valor
+    else:
+      print("Saldo insuficiente para saque.")
+
+  def _str_(self):
+    return f"Número da conta: {self.numero_conta}\nNome do correntista: {self.nome_correntista}\nSaldo: R${self.saldo:.2f}"
