08080afe260885c3bb0460f3754970b602116758
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-13 15:20:10 -0300

    01° Questão.py

--- 6°_Atividade_Python/01° Questão.py
+++ 6°_Atividade_Python/01° Questão.py
@@ -1,1 +1,9 @@
+#1- 1. Faça um programa que possua um Dicionário, adicione elementos ao dicionário e os mostre na tela.
 
+dicionario = {}
+
+dicionario["nome"] = "Fabianny"
+dicionario["idade"] = 17
+dicionario["cidade"] = "Recife"
+
+print(dicionario)
