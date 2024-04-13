bff4d8a3b9c222900c8e2730af55f6b975b359e4
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-13 15:32:49 -0300

    01° Questão.py

--- 7°_Atividade_Python/01° Questão.py
+++ 7°_Atividade_Python/01° Questão.py
@@ -1,1 +1,25 @@
+#1- Crie uma classe que modele um quadrado:
+#Atributos: Tamanho do lado
+#Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
+
+class Quadrado:
+  def _init_(self, lado):
+    self.lado = lado
+
+  def mudar_valor_lado(self, novo_lado):
+    self.lado = novo_lado
+
+  def retornar_valor_lado(self):
+    return self.lado
+
+  def calcular_area(self):
+    return self.lado ** 2
+
+quadrado = Quadrado(5)
+
+quadrado.mudar_valor_lado(30)
+
+print(quadrado.retornar_valor_lado())
+
+print(quadrado.calcular_area())
 
