3319dfc883daaf39c878dfe789508a202ec28587
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-13 15:28:11 -0300

    03° Questão.py

--- 6°_Atividade_Python/03° Questão.py
+++ 6°_Atividade_Python/03° Questão.py
@@ -1,1 +1,12 @@
+#3- 3. Faça um programa, utilizando Dicionários, que peça para o usuário inserir quatro notas e mostre na tela as notas e a média entre elas.
 
+notas = {}
+
+for i in range(4):
+  nota = float(input("Digite a nota: "))
+  notas[f"nota{i+1}"] = nota
+
+media = sum(notas.values()) / len(notas)
+
+print(f"Notas: {notas}")
+print(f"Média: {media}")
