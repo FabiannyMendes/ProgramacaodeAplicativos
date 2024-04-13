d0f6eb34a82f21582b889f87bf045e6fcdf8d584
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-13 15:36:21 -0300

    01° Questão.py

--- 8°_Atividade_Python_/01° Questão.py
+++ 8°_Atividade_Python_/01° Questão.py
@@ -1,1 +1,21 @@
+#1. Crie uma classe que modele uma pessoa
+#(a) Atributos: nome, idade e endereco
+#(b) Metodos: mostrar endereco e alterar endereco
 
+class Pessoa:
+  def _init_(self, nome, idade, endereco):
+    self.nome = nome
+    self.idade = idade
+    self.endereco = endereco
+
+  def mostrar_endereco(self):
+    print(f"Endereço: {self.endereco}")
+
+  def alterar_endereco(self, novo_endereco):
+    self.endereco = novo_endereco
+
+pessoa = Pessoa("Fabianny", 17, "73 Recife")
+
+pessoa.mostrar_endereco()
+pessoa.alterar_endereco("825 Janga")
+pessoa.mostrar_endereco()
