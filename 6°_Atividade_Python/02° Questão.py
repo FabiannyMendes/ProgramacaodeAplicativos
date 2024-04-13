a053ac983361dcbf0debf0be512e2467dec46940
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-13 15:26:06 -0300

    02° Questão.py

--- 6°_Atividade_Python/02° Questão.py
+++ 6°_Atividade_Python/02° Questão.py
@@ -1,1 +1,10 @@
+#2- 2. Faça um programa, utilizando Dicionários, que peça para o usuário inserir o nome de três produtos de mercado e seus respectivos preços e os mostre na tela.
 
+produtos = {}
+
+for i in range(3):
+  nome = input("Digite o nome do produto: ")
+  preco = float(input("Digite o preço do produto: "))
+  produtos[nome] = preco
+
+print(produtos)
