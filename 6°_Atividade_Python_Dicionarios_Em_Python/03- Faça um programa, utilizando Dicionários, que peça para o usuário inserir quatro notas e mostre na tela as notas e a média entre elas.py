fab6589457c7a2f2c397399af87fa93fb6d777c5
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-16 19:13:59 -0300

    03- Faça um programa, utilizando Dicionários, que peça para o usuário inserir quatro notas e mostre na tela as notas e a média entre elas.py

--- 6°_Atividade_Python_Dicionarios_Em_Python/03- Faça um programa, utilizando Dicionários, que peça para o usuário inserir quatro notas e mostre na tela as notas e a média entre elas.py
+++ 6°_Atividade_Python_Dicionarios_Em_Python/03- Faça um programa, utilizando Dicionários, que peça para o usuário inserir quatro notas e mostre na tela as notas e a média entre elas.py
@@ -1,4 +1,4 @@
-#3- 3. Faça um programa, utilizando Dicionários, que peça para o usuário inserir quatro notas e mostre na tela as notas e a média entre elas.
+#3- Faça um programa, utilizando Dicionários, que peça para o usuário inserir quatro notas e mostre na tela as notas e a média entre elas.
 
 notas = {}
 
