f808ebe3d6602b9b6b765ba85f6785ab118a9269
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-16 07:38:45 -0300

    16° Questão.py

--- 1°_Atividade_Python/16° Questão.py
+++ 1°_Atividade_Python/16° Questão.py
@@ -1,4 +1,4 @@
-#17 - Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), 
+#16 - Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), 
 #calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 
 tamanho_arquivo = float(input("Digite o tamanho do arquivo para download em MB: "))
