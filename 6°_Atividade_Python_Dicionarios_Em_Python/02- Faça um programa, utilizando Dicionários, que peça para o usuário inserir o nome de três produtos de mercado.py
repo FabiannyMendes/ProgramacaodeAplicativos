b592b55d1cba6fb33369709cc399920ab363a0d0
Fabianny Mendes <127613317+FabiannyMendes@users.noreply.github.com>
2024-04-16 19:12:59 -0300

    02- Faça um programa, utilizando Dicionários, que peça para o usuário inserir o nome de três produtos de mercado.py

--- 6°_Atividade_Python_Dicionarios_Em_Python/02- Faça um programa, utilizando Dicionários, que peça para o usuário inserir o nome de três produtos de mercado.py
+++ 6°_Atividade_Python_Dicionarios_Em_Python/02- Faça um programa, utilizando Dicionários, que peça para o usuário inserir o nome de três produtos de mercado.py
@@ -1,4 +1,4 @@
-#2- 2. Faça um programa, utilizando Dicionários, que peça para o usuário inserir o nome de três produtos de mercado e seus respectivos preços e os mostre na tela.
+#2- Faça um programa, utilizando Dicionários, que peça para o usuário inserir o nome de três produtos de mercado e seus respectivos preços e os mostre na tela.
 
 produtos = {}
 
